7e32cc8673e16b0202283406850c62eb6ad8c9be
Rodrigo Corrêa <rcelha@gmail.com>
2016-12-19 01:26:26 +0000

    Add desc to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
+#!/usr/bin/env python
+
 from setuptools import setup, find_packages
+from os import path
+
+cwd = path.dirname(path.realpath(__file__))
+with open(path.join(cwd, 'README.md')) as f:
+    long_description = f.read()
 
 setup(
   name="textteaser",
   version="1.0.0",
+  description='TextTeaser is an automatic summarization algorithm',
+  long_description=long_description,
+  url='https://github.com/rcelha/textteaser',
+  install_requires=['nltk'],
   packages=find_packages(exclude=['contrib', 'docs', 'tests'])
 )
