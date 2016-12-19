a0da6b214180a316278c0d0d49ce0bd619842780
Rodrigo Corrêa <rcelha@gmail.com>
2016-12-19 01:17:29 +0000

    Add packages to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
   name="textteaser",
   version="1.0.0",
+  packages=find_packages(exclude=['contrib', 'docs', 'tests'])
 )
